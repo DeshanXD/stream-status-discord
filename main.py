8c6e520ee84f5bb6248443dee39d5917149f2e47
Deshan Basura <30923476+DeshanXD@users.noreply.github.com>
2022-07-10 12:06:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ client = commands.Bot(
 @client.event
 async def on_connect():
   await client.change_presence(activity = discord.Streaming(name = 
-  "status", url = "https://twitch.tv/paogah"))
+  "Streaming Among Us!!", url = "https://twitch.tv/desiregonelive"))
 
 
 
